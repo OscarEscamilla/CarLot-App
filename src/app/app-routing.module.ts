1c08a9e6be4cff3ddd7a60a7ec47b52320ac0cf7
unknown <ozkarezkamiya26qgmail.com>
2019-11-03 22:43:27 -0600

    comienzo de interfaces

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,15 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: 'inicio', loadChildren: './pages/inicio/inicio.module#InicioPageModule' },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'tabs', loadChildren: './pages/tabs/tabs.module#TabsPageModule' },
+  { path: 'tab1', loadChildren: './pages/tab1/tab1.module#Tab1PageModule' },
+  { path: 'tab2', loadChildren: './pages/tab2/tab2.module#Tab2PageModule' },
+  { path: 'tab3', loadChildren: './pages/tab3/tab3.module#Tab3PageModule' },
+  { path: 'tab4', loadChildren: './pages/tab4/tab4.module#Tab4PageModule' },
+  
 ];
 
 @NgModule({
---
+++ src/app/componentes/micomponente/micomponente.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-micomponente',
+  templateUrl: './micomponente.component.html',
+  styleUrls: ['./micomponente.component.scss'],
+})
+export class MicomponenteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
